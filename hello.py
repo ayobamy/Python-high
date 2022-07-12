89d16563439d15d4e9b06d99eeb8f4839e60fc89
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-07-12 12:23:06 +0000

    practice

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+class Employee:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+emp_1 = Employee('Ahmed', 24)
+emp_2 = Employee('Olawale', 28)
+emp_3 = Employee('Female', 29)  
+
+print(emp_1.name)
